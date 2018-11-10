1e8d467da89a9dc5006300114fbc56351c06a844
Dejan <dekimioc@yahoo.com>
2018-11-10 15:02:52 +0100

    Added few components and some CSS styling

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Meat', type: 'meat'}
+]
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label}/>
+        ))}
+    </div>
+)
+
+export default buildControls;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css'
 
 const layout = (props) => (
     <Auxiliary>
         <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxiliary>
