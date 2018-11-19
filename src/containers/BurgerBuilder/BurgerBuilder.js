93f2230c04013fc8fdc662d2bbf93837f198cddd
Dejan <dekimioc@yahoo.com>
2018-11-19 22:29:22 +0100

    Adding reducer and action types

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,12 +33,12 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://my-burger-app-react-de643.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ingredients: response.data})
-            }).catch(error => {
-                this.setState({error: true})
-            });
+        // axios.get('https://my-burger-app-react-de643.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ingredients: response.data})
+        //     }).catch(error => {
+        //         this.setState({error: true})
+        //     });
     }
 
     updateStatePurchasable (ingredients) {
