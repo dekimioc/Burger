82d455c99a894d9e686ea2f295f70c8feb416bb3
Dejan Miocinovic <dekimioc@yahoo.com>
2020-02-27 22:50:55 +0100

    Updates

---
+++ precache-manifest.176cb0dab63f6624821b54c3baf8ce15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93c6e6c2bab9198e21eb",
+    "url": "/burger/static/css/main.995295fd.chunk.css"
+  },
+  {
+    "revision": "93c6e6c2bab9198e21eb",
+    "url": "/burger/static/js/main.93c6e6c2.chunk.js"
+  },
+  {
+    "revision": "b7b333e659f8a3d68599",
+    "url": "/burger/static/js/1.b7b333e6.chunk.js"
+  },
+  {
+    "revision": "e4f707eb8257d40b61ad",
+    "url": "/burger/static/js/runtime~main.e4f707eb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "480119f1eb1f95a0e32b930cd0617a04",
+    "url": "/burger/index.html"
+  }
+];
